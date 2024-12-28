4585e7aa6a873819ff7278d0327d3151a510d6bd
Israt85 <isratanjuman3@gmail.com>
2024-12-28 22:28:28 +0600

    added chat

---
+++ src/Pages/Message/ChatBubble.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+const ChatBubble = () => {
+    return (
+        <div>
+            <div>
+            <div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://s3-alpha-sig.figma.com/img/61a0/b47a/7c5376560de2a64cb611049443695aae?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hF0pKxsl9fK8Q45gcM7iBpKKeRKtInPqvEa1CI9ocfNUCDNh8E9zceaGi5jypGHUX6PTnxI9D3~aAP9nZ~Tri4dPtq2yaPLvjXpto9FihWmHPOwWr7lyIyOw5XDRiGkMWc7r6wq-f2rOxEPx1YWmRqX~f60uRssghi41dgY901KdFdyjcNgvHWKIaYO9-ZPIJ~JnvSHZcjRBaFtpOUvka~Jo0l-NRbwL0ObUst0EDFdVktTte9LrHhCbXN4qU-zp-AuaDFCqMQ80B2PyQ60iYjfPtlma2O4fiaPzIOVOzdhN2gH95py5xcNFbKvIFDTmuZ8JY4cAzwGd0aUC1B6vOA__" />
+    </div>
+  </div>
+  <div className="chat-header">
+  Roselle Ehrman
+    <time className="text-xs opacity-50 ml-4">08:45 AM</time>
+  </div>
+  <div className="chat-bubble bg-[#F5F5F5] text-black">Hi! How are you?</div>
+  <div className="chat-footer opacity-50"></div>
+</div>
+<div className="chat chat-end">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://s3-alpha-sig.figma.com/img/1363/ce47/f689bda1937d8c2d1741c1c37f9f2999?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qFp5BzjZXKM~aa4JLtovyOpOhpMsR0vfQA7yk5RebQeeRIzXLG0q-9JjU-EXPOZEp6EF6DLyoihfNA4DLA19gxKwN-tFa3FTI~mZlrHXkdaUYGdCC9-2li4AmUBKjVbZGuOGepm~pib12MphWx8mWZzoRU12MVam1xGfF-OriRr5OfpuoOORU1Q~2It58Hz0tdqlkqRgtjRAYxuCc0ZYGziw7Byw~afBzDvyaw13WwpmR9jOldAKgChZlPkrUvSbgq4jTAQ-snxk7VaJjUYK1a3ahoJtnHDqlPNNrJFrTawqlaEqkxrQKHIpEwOIEOxcebl6fwbby7HOaL~5GBHJxw__" />
+    </div>
+  </div>
+  <div className="chat-header">
+    
+    <time className="text-xs opacity-50 mr-4">08:45 AM</time>
+    You
+  </div>
+  <div className="chat-bubble bg-[#F5F5F5] w-[280px] text-black">Hey Roselle! I’m feel amazing
+  how about you?</div>
+</div>
+        </div>
+        <div>
+            <div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://s3-alpha-sig.figma.com/img/61a0/b47a/7c5376560de2a64cb611049443695aae?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hF0pKxsl9fK8Q45gcM7iBpKKeRKtInPqvEa1CI9ocfNUCDNh8E9zceaGi5jypGHUX6PTnxI9D3~aAP9nZ~Tri4dPtq2yaPLvjXpto9FihWmHPOwWr7lyIyOw5XDRiGkMWc7r6wq-f2rOxEPx1YWmRqX~f60uRssghi41dgY901KdFdyjcNgvHWKIaYO9-ZPIJ~JnvSHZcjRBaFtpOUvka~Jo0l-NRbwL0ObUst0EDFdVktTte9LrHhCbXN4qU-zp-AuaDFCqMQ80B2PyQ60iYjfPtlma2O4fiaPzIOVOzdhN2gH95py5xcNFbKvIFDTmuZ8JY4cAzwGd0aUC1B6vOA__" />
+    </div>
+  </div>
+  <div className="chat-header">
+  Roselle Ehrman
+    <time className="text-xs opacity-50 ml-4">08:45 AM</time>
+  </div>
+  <div className="chat-bubble bg-[#F5F5F5] text-black">Hi! How are you?</div>
+  <div className="chat-footer opacity-50"></div>
+</div>
+<div className="chat chat-end">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://s3-alpha-sig.figma.com/img/1363/ce47/f689bda1937d8c2d1741c1c37f9f2999?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qFp5BzjZXKM~aa4JLtovyOpOhpMsR0vfQA7yk5RebQeeRIzXLG0q-9JjU-EXPOZEp6EF6DLyoihfNA4DLA19gxKwN-tFa3FTI~mZlrHXkdaUYGdCC9-2li4AmUBKjVbZGuOGepm~pib12MphWx8mWZzoRU12MVam1xGfF-OriRr5OfpuoOORU1Q~2It58Hz0tdqlkqRgtjRAYxuCc0ZYGziw7Byw~afBzDvyaw13WwpmR9jOldAKgChZlPkrUvSbgq4jTAQ-snxk7VaJjUYK1a3ahoJtnHDqlPNNrJFrTawqlaEqkxrQKHIpEwOIEOxcebl6fwbby7HOaL~5GBHJxw__" />
+    </div>
+  </div>
+  <div className="chat-header">
+    
+    <time className="text-xs opacity-50 mr-4">08:45 AM</time>
+    You
+  </div>
+  <div className="chat-bubble bg-[#F5F5F5] w-[280px] text-black">Hey Roselle! I’m feel amazing
+  how about you?</div>
+</div>
+        </div>
+        </div>
+    );
+};
+
+export default ChatBubble;
--- src/Pages/Message/Message.jsx
+++ src/Pages/Message/Message.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ChatBubble from './ChatBubble';
 
 const Message = () => {
     return (
@@ -9,14 +10,22 @@ const Message = () => {
 
                 </div>
                 <div className='w-[690px] h-[800px] border'>
-                     <p className='w-full border-b-2 h-20'>
-                        <img className='w-16 h-16 rounded-full' src="https://s3-alpha-sig.figma.com/img/61a0/b47a/7c5376560de2a64cb611049443695aae?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hF0pKxsl9fK8Q45gcM7iBpKKeRKtInPqvEa1CI9ocfNUCDNh8E9zceaGi5jypGHUX6PTnxI9D3~aAP9nZ~Tri4dPtq2yaPLvjXpto9FihWmHPOwWr7lyIyOw5XDRiGkMWc7r6wq-f2rOxEPx1YWmRqX~f60uRssghi41dgY901KdFdyjcNgvHWKIaYO9-ZPIJ~JnvSHZcjRBaFtpOUvka~Jo0l-NRbwL0ObUst0EDFdVktTte9LrHhCbXN4qU-zp-AuaDFCqMQ80B2PyQ60iYjfPtlma2O4fiaPzIOVOzdhN2gH95py5xcNFbKvIFDTmuZ8JY4cAzwGd0aUC1B6vOA__" alt="" />
-                        <div className="badge badge-primary badge-xs"></div>
+                     <p className='w-full flex px-6 py-4 items-center gap-2 border-b-2 h-24'>
+                       <div className='relative'>
+                       <img className='w-16 h-16 rounded-full' src="https://s3-alpha-sig.figma.com/img/61a0/b47a/7c5376560de2a64cb611049443695aae?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hF0pKxsl9fK8Q45gcM7iBpKKeRKtInPqvEa1CI9ocfNUCDNh8E9zceaGi5jypGHUX6PTnxI9D3~aAP9nZ~Tri4dPtq2yaPLvjXpto9FihWmHPOwWr7lyIyOw5XDRiGkMWc7r6wq-f2rOxEPx1YWmRqX~f60uRssghi41dgY901KdFdyjcNgvHWKIaYO9-ZPIJ~JnvSHZcjRBaFtpOUvka~Jo0l-NRbwL0ObUst0EDFdVktTte9LrHhCbXN4qU-zp-AuaDFCqMQ80B2PyQ60iYjfPtlma2O4fiaPzIOVOzdhN2gH95py5xcNFbKvIFDTmuZ8JY4cAzwGd0aUC1B6vOA__" alt="" />
+                       <div className="absolute top-0 left-12 badge badge-secondary badge-xs"></div>
+                       </div>
                         <p>
                             <p>Roselle Ehrman</p>
-                            <p>online</p>
+                            <p className='text-[#EC008C]'>online</p>
                         </p>
                      </p>
+                     <div className='flex justify-center my-6'>
+                        <p className='w-32 flex items-center justify-center h-10 text-white bg-[#27D095] rounded-full'>yesterday</p>
+                     </div>
+                     <div>
+                        <ChatBubble></ChatBubble>
+                     </div>
                 </div>
             </div>
         </div>
